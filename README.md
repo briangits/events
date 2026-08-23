cc5b9cb65c5a73295f68964c2e9dc834f0344b45
briangits <briangits@proton.me>
2026-08-23 21:57:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# events
+
+An in-process event bus
+
+## License
+
+[Apache-2.0](LICENSE)
---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    // Platforms
+    alias(kt.plugins.multiplatform)
+    alias(kt.plugins.multiplatform.android)
+}
+
+group = "io.github.briangits"
+version = "0.0.1"
+
+kotlin {
+    jvm()
+
+    js {
+        browser()
+        nodejs()
+    }
+
+    android {
+        namespace = "$group"
+        compileSdk = 37
+        minSdk = 2
+
+        compilerOptions {
+            jvmTarget = JvmTarget.JVM_11
+        }
+    }
+}
