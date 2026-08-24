480a842be64396010a2dba324be74535cf261c9c
briangits <briangits@proton.me>
2026-08-24 12:45:30 +0300

    feat: configure maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     // Platforms
     alias(kt.plugins.multiplatform)
     alias(kt.plugins.multiplatform.android)
+
+    // Publishing
+    alias(libutils.plugins.vanniktech)
 }
 
 group = "io.github.briangits"
@@ -38,3 +41,41 @@ kotlin {
         }
     }
 }
+
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+
+    coordinates(group.toString(), "events", version.toString())
+
+    pom {
+        name = "events"
+        description = "An in-process event bus"
+        inceptionYear = "2026"
+
+        url = "https://github.com/briangits/events"
+
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+
+        developers {
+            developer {
+                id = "briangits"
+                name = "Gits"
+                url = "https://github.com/briangits"
+            }
+        }
+
+        scm {
+            url = "https://github.com/briangits/events"
+            connection = "scm:git:git://github.com/briangits/events.git"
+            developerConnection = "scm:git:git://github.com/briangits/events.git"
+        }
+    }
+}
+
